e4d76ca419a780092c9e78db8e4e07593c60cde1
Marcos Guimaraes <marcos@agr-sistemas.com.br>
2020-01-28 09:40:10 -0300

    Ajustes...
    Signed-off-by: Marcos Guimaraes <marcos@agr-sistemas.com.br>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,10 +51,14 @@
                         @else
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('home')}}">Home</a>
-                        </li>                         
+                        </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('contacts.index')}}">Contact</a>
                         </li> 
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('contacts-doctrine.index')}}">Contact-Doctrine</a>
+                        </li> 
+
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
